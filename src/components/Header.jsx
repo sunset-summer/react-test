87808d3c79ff367cd8e181749b418321a8eb42d0
sunset <alex1123321@gmail.com>
2020-10-08 16:33:18 +0300

    Init

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img src='https://upload.wikimedia.org/wikipedia/commons/a/ac/Oikya_Front_Logo.png'></img>
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className='content'>
+  <div>
+    <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"></img>
+  </div>
+  <div>
+    ava + description
+  </div>
+  <div>
+    my posts
+    <div>
+      new post
+    </div>
+    <div>
+      post 1
+    </div>
+    <div>
+      post 2
+    </div>
+  </div>
+  </div>
+}
+
+export default Profile;
