6877e8d0dc82b4cb62091f53df48da1fdabfbd8f
sunset <alex1123321@gmail.com>
2020-10-09 01:58:58 +0300

    added css.modules

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import S from './Nav.module.css';
 
 const Nav = () => {
-  return <nav className='nav'>
-    <div>
+  return <nav className={S.nav}>
+    <div className={S.item}>
       <a>Profile</a>
     </div>
-    <div>
+    <div className={S.item}>
       <a>Messages</a>
     </div>
-    <div>
+    <div className={S.item}>
       <a>News</a>
     </div>
-    <div>
+    <div className={S.item}>
       <a>Music</a>
     </div>
-    <div>
+    <div className={S.item}>
       <a>Settings</a>
     </div>
   </nav>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
-  return <div className='content'>
+  return <div className={s.content}>
   <div>
     <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"></img>
   </div>
